a1e6bac88cfe81927389e9fef741680841132046
michaelsatumba <mjsatumba@gmail.com>
2024-09-12 14:04:29 -0700

    padding

--- src/views/IndividualView.js
+++ src/views/IndividualView.js
@@ -66,71 +66,36 @@ Contributed to the offsite idea for H2 2024</li>
 
 {/* Flex */}
 <div className="flex space-x-4 mb-4">
-    
+
 {/* Medium Rectangle */}
-<div className="flex-grow bg-white dark:bg-gray-700 p-4 shadow rounded-lg">
-<div className="flex items-center mb-2">
-    <i className="fas fa-user mr-4"></i>
-    <div className="flex flex-col items-start">
-    <p className="font-bold">Aaron Jones</p>
-    <p className="text-gray-500 text-sm">Software Engineer</p>
+<div className="flex-grow bg-white dark:bg-gray-700 p-4 shadow rounded-lg pr-64">
+    <div className="flex items-center justify-between mb-2">
+        <i className="fab fa-github"></i>
+        <div>
+            <p>+5</p>
+        </div>
     </div>
-</div>
-<ul className="list-none mb-2 text-left font-semibold text-sm">
-    <li className="relative pl-3 mb-4">
-    <span className="absolute left-0 top-0 text-gray-500">•</span>
-    Added alert on failed fetch data error
-    </li>
-    <li className="relative pl-3 mb-4">
-    <span className="absolute left-0 top-0 text-gray-500">•</span>
-    Updated runbook for failed fetch data alert
-    </li>
-    <li className="relative pl-3 mb-4">
-    <span className="absolute left-0 top-0 text-gray-500">•</span>
-    Attended #auth-api meeting to provide context for the front-end teams
-    </li>
-    <li className="relative pl-3 mb-4">
-    <span className="absolute left-0 top-0 text-gray-500">•</span>
-    Updated the auth API documentation
-    </li>
-</ul>
-<div className="flex space-x-4">
-<p><i class="fab fa-github"></i> 4</p>
-<p><i class="fab fa-slack"></i> 45</p>
-<p><i class="fas fa-video"></i> 45m</p>
-<p><i class="fas fa-user-friends"></i></p>
-</div>
+    <p className="flex font-bold">oauth</p>
+    <p className="text-gray-500 text-left text-xs">Changes on inactive user storage management</p>
+    <br/>
+    <div className="flex space-x-4 text-xs">
+        <p><i class="fas fa-arrow-up pr-1"></i>1</p>
+        <p><i class="fas fa-arrow-down pr-1"></i>2</p>
+        <p><i class="fas fa-arrow-left pr-1"></i>3</p>
+        <p><i class="fas fa-arrow-right pr-1"></i>1</p>
+    </div>
+    <div className="flex space-x-4 mt-6">
+        <p><i class="fas fa-user-friends"></i></p>
+        <p classname="text-sm text-gray-500">60m ago</p>
+    </div>
+
 </div>
 {/* Medium Rectangle */}
-<div className="flex-grow bg-white dark:bg-gray-700 p-4 shadow rounded-lg">
-<div className="flex items-center mb-2">
-    <i className="fas fa-user mr-4"></i>
-    <div className="flex flex-col items-start">
-    <p className="font-bold">Christine Li</p>
-    <p className="text-gray-500 text-sm">Staff Software Engineer</p>
-    </div>
+<div className="w-1/4">
+ <img src="https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg" alt="Dog" className="w-full h-auto rounded-lg" />
 </div>
-<ul className="list-none mb-2 text-left font-semibold text-sm">
-    <li className="relative pl-3 mb-4">
-    <span className="absolute left-0 top-0 text-yellow-500">•</span>
-    Facilitated the Inactive user token and storage discussion meeting
-    </li>
-    <li className="relative pl-3 mb-4">
-    <span className="absolute left-0 top-0 text-yellow-500">•</span>
-    Gave a shoutout to <u>Michael</u> for his work on debugging the prod bug for failed to fetch data
-    </li>
-    <li className="relative pl-3 mb-4">
-    <span className="absolute left-0 top-0 text-yellow-500">•</span>
-    Closed discussion on storage plan for inactive user with <u>Aaron</u> and <u>Alexey</u>
-    </li>
-</ul>
-<div className="flex space-x-4">
-<p><i class="fab fa-github"></i> 13</p>
-<p><i class="fab fa-slack"></i> 36</p>
-<p><i class="fas fa-video"></i> 75m</p>
-<p><i class="fas fa-user-friends"></i></p>
-</div>
-</div>
+
+
 </div>
 
 {/* Flex */}
